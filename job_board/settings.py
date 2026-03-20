13c53088a3bb15ce113219d25c687a19b63c273e
CharlesDesc06 <charles.deschamps06@hotmail.com>
2026-03-20 14:33:16 +0100

    Add Docker Compose configuration for web service

--- job_board/settings.py
+++ job_board/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-v++r+fn6w$j+yr3ye5+_qnqepn*h(n@i$-@hem%b=t3@4za%wp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    os.getenv("APP_DOMAIN", "localhost"),
+]
 
 
 # Application definition
